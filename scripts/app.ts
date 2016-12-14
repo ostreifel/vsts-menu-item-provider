601f7badb51882bfa6e626547c28048f1ff02ab5
Otto Streifel <ottost@microsoft.com>
2016-12-14 09:51:38 -0800

    v0.1.25 sometimes disable menu, add action

--- scripts/app.ts
+++ scripts/app.ts
@@ -3,10 +3,12 @@ let callCount = 0;
 // will be recomputed each time the work item changes
 const menuContributionHandler: IContributedMenuSource = {
     getMenuItems: function (context): (IContributedMenuItem & {dynamic?: boolean})[] {
-        // could be a promise instead
+        // could also be a promise  
         return [{
             title: "Menu Items Group",
             text: `Menu item Name 1, generated ${++callCount} times`,
+            disabled: callCount % 3 === 0,
+            action: context => alert("executing my action"),
             // icon: "img/icon.png",
             dynamic: true
         }];
