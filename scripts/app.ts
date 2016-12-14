b7209583fa807c2b006ed100c21ff8a45ab230c9
Otto Streifel <ottost@microsoft.com>
2016-12-14 11:47:09 -0800

    v0.1.29 Updates to disable on title !== 'Enabled'

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,18 +1,51 @@
+import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
+import { IWorkItemNotificationListener } from "TFS/WorkItemTracking/ExtensionContracts";
+
+
 let callCount = 0;
+interface IMenuItemsContext {
+    closeWorkItemFormDialogDelegate?: Function;
+    currentProjectGuid: string;
+    currentProjectName: string;
+    hideDelete: false;
+    movedToNewProject: false;
+    tfsContext: any;
+    updateMenuItems: (menuItems: IContributedMenuItem[]) => IPromise<void>;
+    workItemAvailable: boolean;
+    workItemDirty: boolean;
+    workItemId: number;
+    workItemTypeName: string;
+}
+
+let menuItemsContext: IMenuItemsContext;
+function getMenuItems(): IPromise<IContributedMenuItem & { dynamic?: boolean }[]> {
+    return WorkItemFormService.getService().then(service => {
+        return service.getFieldValue("System.Title").then(val => {
+            const menuItems = [{
+                title: "Menu Items Group",
+                text: `Menu item Name 1, generated ${++callCount} times`,
+                disabled: val !== "Enabled",
+                action: context => alert("executing my action"),
+                // icon: "img/icon.png",
+                dynamic: true,
+            }];
+            return menuItems;
+        });
+    });
+}
 
 // will be recomputed each time the work item changes
-const menuContributionHandler: IContributedMenuSource = {
-    getMenuItems: function (context): (IContributedMenuItem & {dynamic?: boolean})[] {
-        // could also be a promise  
-        return [{
-            title: "Menu Items Group",
-            text: `Menu item Name 1, generated ${++callCount} times`,
-            disabled: callCount % 3 === 0,
-            action: context => alert("executing my action"),
-            // icon: "img/icon.png",
-            dynamic: true
-        }];
+const menuContributionHandler: IContributedMenuSource & Partial<IWorkItemNotificationListener> = {
+    getMenuItems: function (context: IMenuItemsContext) {
+        menuItemsContext = context;
+        return getMenuItems();
+    },
+    onFieldChanged: context => {
+        getMenuItems().then(menuItems =>
+            menuItemsContext && menuItemsContext.updateMenuItems(menuItems)
+        );
     }
 };
+
 
 VSS.register(VSS.getContribution().id, menuContributionHandler);
